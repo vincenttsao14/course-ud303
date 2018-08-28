2b4507ba51f2de15cd3f064ed2e9f93452e78825
Vincent Tsao <vincenttsao14@gmail.com>
2018-08-28 17:51:52 -0500

    EchoHandler update

--- Lesson-2/1_EchoServer/EchoServer.py
+++ Lesson-2/1_EchoServer/EchoServer.py
@@ -19,7 +19,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-class HelloHandler(BaseHTTPRequestHandler):
+class EchoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         # First, send a 200 OK response.
         self.send_response(200)
@@ -28,10 +28,12 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
 
+        path = self.path
+
         # Now, write the response body.
-        self.wfile.write("Hello, HTTP!\n".encode())
+        self.wfile.write(path.encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)  # Serve on all addresses, port 8000.
-    httpd = HTTPServer(server_address, HelloHandler)
+    httpd = HTTPServer(server_address, EchoHandler)
     httpd.serve_forever()
